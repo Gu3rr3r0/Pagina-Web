10f1838c4f74e1b8c2a30e20156232618b88122a
Gu3rr3r0 <80969685+Gu3rr3r0@users.noreply.github.com>
2021-07-06 00:45:30 -0400

    Delete newproyectosemestral directory

--- newproyectosemestral/paginaweb/admin.py
+++
@@ -1,9 +0,0 @@
-from paginaweb.views import contactanos
-from django.contrib import admin
-from .models import Usuario, Producto, OrdenCompra, Contactanos
-# Register your models here.
-
-admin.site.register(Usuario)
-admin.site.register(Producto)
-admin.site.register(OrdenCompra)
-admin.site.register(Contactanos)
